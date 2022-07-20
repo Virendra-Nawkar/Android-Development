6ad1565bb5511b9d40e86fa0702543c8a72d7352
Virendra-Nawkar <virendranawkar1@gmail.com>
2022-07-20 23:00:37 +0530

    first commit

---
+++ Hello/Demo.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Demo
+{
+	public static void main (String agrs[])
+	 {
+		System.out.println("Helloooooooo World");	
+	}
+}
---
+++ Hello/Input.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Input {
+    public static void main(String[] args) {
+        String s = JOptionPane.showInputDialog(null, "Enter A Number");
+        System.out.println(s);
+    }
+}
